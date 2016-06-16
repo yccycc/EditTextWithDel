f14f01994e3eb8521a045cfa24ba31932f5866ef
ycc <523810076@qq.com>
2016-06-16 15:50:42 +0800

    增强可读性

--- app/src/main/java/com/example/dicky/superedittext/SuperEditText.java
+++ app/src/main/java/com/example/dicky/superedittext/SuperEditText.java
@@ -6,26 +6,15 @@ import android.graphics.drawable.Drawable;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.EditText;
 
 public class SuperEditText extends EditText {
-    private final static String TAG = "EditTextWithDel";
-    private Drawable imgInable;
-    private Drawable imgAble;
+    private Drawable delDrawable;
     private Context mContext;
 
     public SuperEditText(Context context) {
-        super(context);
-        mContext = context;
-        init();
-    }
-
-    public SuperEditText(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        mContext = context;
-        init();
+        super(context, null);
     }
 
     public SuperEditText(Context context, AttributeSet attrs) {
@@ -35,8 +24,7 @@ public class SuperEditText extends EditText {
     }
 
     private void init() {
-        imgInable = mContext.getResources().getDrawable(R.mipmap.ins_search_black_pr);
-        imgAble = mContext.getResources().getDrawable(R.mipmap.ins_search_black);
+        delDrawable = mContext.getResources().getDrawable(R.mipmap.ins_search_black);
         addTextChangedListener(new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
@@ -57,18 +45,17 @@ public class SuperEditText extends EditText {
     //设置删除图片
     private void setDrawable() {
         if (length() < 1)
-            setCompoundDrawablesWithIntrinsicBounds(null, null, imgInable, null);
+            setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
         else
-            setCompoundDrawablesWithIntrinsicBounds(null, null, imgAble, null);
+            setCompoundDrawablesWithIntrinsicBounds(null, null, delDrawable, null);
     }
 
     // 处理删除事件
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (imgAble != null && event.getAction() == MotionEvent.ACTION_UP) {
+        if (delDrawable != null && event.getAction() == MotionEvent.ACTION_UP) {
             int eventX = (int) event.getRawX();
             int eventY = (int) event.getRawY();
-            Log.e(TAG, "eventX = " + eventX + "; eventY = " + eventY);
             Rect rect = new Rect();
             getGlobalVisibleRect(rect);
             rect.left = rect.right - 50;
@@ -77,10 +64,4 @@ public class SuperEditText extends EditText {
         }
         return super.onTouchEvent(event);
     }
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-    }
-
 }
